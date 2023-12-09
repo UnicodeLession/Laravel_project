6ddfd98367dcf0ed77a18b556feb4565a39c9255
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-12-09 16:37:26 +0700

    Admin Front Page: logout action

--- public/backend/js/custom.js
+++ public/backend/js/custom.js
@@ -84,3 +84,15 @@ if(slug){
 }
 //unisharp.github.io/laravel-filemanager/integration#standalone-button
 $('#lfm').filemanager('image');
+
+const logoutAction = document.querySelector('.logout-action');
+const logoutForm = document.querySelector('.logout-form');
+
+if (logoutAction && logoutForm){
+    logoutAction.addEventListener('click', (e)=>{
+        e.preventDefault();
+        logoutForm.action = e.target.href;
+
+        logoutForm.submit();
+    })
+}
--- resources/views/parts/backend/header.blade.php
+++ resources/views/parts/backend/header.blade.php
@@ -219,7 +219,10 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary logout-action" href="{{route('logout')}}">Logout</a>
+                    <form method="post" class="logout-form d-none">
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
