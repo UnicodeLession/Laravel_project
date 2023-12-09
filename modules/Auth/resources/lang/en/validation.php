6ddeaeb44f8f4682337a20609c8cc5913b3cb3fb
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-12-09 16:06:37 +0700

    Admin Front Page: integrate laravel UI Authentication for login and use SBAdmin's login form

---
+++ modules/Auth/resources/lang/en/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'required' => ':attribute bắt buộc phải nhập',
+    'email' => ':attribute phải là chuỗi',
+    'attributes' => [
+        'email' => 'Email',
+        'password' => 'Mật khẩu',
+    ]
+];
---
+++ modules/Auth/resources/views/admin/login.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.auth')
+
+@section('content')
+    <div class="row justify-content-center" >
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back! Administrator</h1>
+                                </div>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror"
+                                               id="email" aria-describedby="emailHelp" name="email" value="{{ old('email') }}"
+                                               placeholder="Enter Email Address...">
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror"
+                                               id="password" placeholder="Password" name="password">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+{{--                                   admin thì k cần--}}
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" name="remember" id="customCheck" class="custom-control-input" {{ old('remember') ? 'checked' : false }}>
+                                            <label class="custom-control-label" for="customCheck" style="padding-top: 3px;">Remember
+                                                Me</label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                    <hr>
+                                    <a href="#" class="btn btn-google btn-user btn-block">
+                                        <i class="fab fa-google fa-fw"></i> Login with Google
+                                    </a>
+                                    <a href="#" class="btn btn-facebook btn-user btn-block">
+                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                    </a>
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                    @if (Route::has('password.request'))
+                                        <a class="small" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+@endsection
+@section('scripts')
